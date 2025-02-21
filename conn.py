5cd91c65abaf9b1be94a9f8b7b509167c9f9d9c3
eduarell-cmd <edu.arellanes9@gmail.com>
2025-02-20 22:30:37 -0600

    .

--- conn.py
+++ conn.py
@@ -1,20 +1,14 @@
-import os
-
-def get_db_connection(): 
-   try:
-      DB_HOST = "localhost"
-      DB_USER = "root"      
-      DB_PASSWORD = ""       
-      DB_NAME = "carros"
-      
-      SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
-      SQLALCHEMY_TRACK_MODIFICATIONS = False
-      
-      print(f"Hola")
-      return
-   except Exception as e :
-      print(f"Valio: {e}")
-
-
-connection = get_db_connection()
-connection = get_db_connection()
+import pymysql
+def get_db_connection():
+    try:
+        conn = pymysql.connect(
+            host="localhost",
+            user="root",
+            password="",
+            database="carros",
+            cursorclass=pymysql.cursors.DictCursor  # Devuelve resultados en formato diccionario
+        )
+        return conn
+    except Exception as e:
+        print(f"Error de conexión: {e}")
+        return None
