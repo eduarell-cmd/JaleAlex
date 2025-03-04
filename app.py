92ca35f6ba2b1bd681beb57cb52aeded6c16f118
Daniel Beltran <danielbeltran468jb@gmail.com>
2025-03-04 10:26:32 -0600

    jalealez

--- app.py
+++ app.py
@@ -44,6 +44,30 @@ task_queue = queue.Queue()
 queue = []
 waitlist = []
 
+class Queue:
+    def __init__(self):
+        self.queue = []
+        self.waitlist = []
+
+    def enqueue(self, item):
+        if len(self.queue) < 10:
+            self.queue.append(item)
+        else:
+            self.waitlist.append(item)
+
+    def dequeue(self):
+        if len(self.queue) > 0:
+            self.queue.pop(0)
+            if len(self.waitlist) > 0:
+                self.queue.append(self.waitlist.pop(0))
+
+    def get_queue(self):
+        return self.queue
+
+    def get_waitlist(self):
+        return self.waitlist
+        
+
 @app.route('/queue',methods=['POST'])
 def queue(name):
     if len(queue) < 10:
